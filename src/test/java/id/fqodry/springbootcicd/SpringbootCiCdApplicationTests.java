5618599b8b7849944da586f8bdf22b8600546458
Moh. Firman Qodry A <firman.qodry@gmail.com>
2021-05-05 14:35:47 +0700

    initiate spring project

---
+++ src/test/java/id/fqodry/springbootcicd/SpringbootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package id.fqodry.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
